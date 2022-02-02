a42ed322eef8a432602a8fb2f4676bd2493624b1
aaronizzl3 <58277262+aaronizzl3@users.noreply.github.com>
2022-02-02 11:04:35 +0000

    Jedi Knight

---
+++ 1 - Module Introduction/Input.py
@@ -0,0 +1,11 @@
+
+firstName = input("First name: ")
+lastName = input("Last name: ")
+print(firstName + " " + lastName)
+
+# Storing in a new variable
+fullName = firstName + " " + lastName
+print(fullName)
+
+# Formatted String
+print(f"{firstName} {lastName}")
---
+++ 1 - Module Introduction/Variables Operators Data Types.py
@@ -0,0 +1,37 @@
+
+# 1 - Variables and Data Types
+myString = "Hello World!"
+myInteger = 10
+myBoolean = True
+myFloat = 5.5
+
+# 2 - Printing Values
+print(myString)
+print(myInteger)
+print("Aaron Hussain")
+
+# 3 - Operators (+ - / *)
+numA = 6
+numB = 2
+
+addition = numA + numB
+print(addition)
+print(numA + numB)
+
+# MODULUS
+print(3 % 2)
+
+# Increment / Decrement
+x = 5
+x += 1
+x -= 1
+
+# Strings Adding - Concatenation
+wordOne = "Hello"
+wordTwo = "World"
+print(wordOne + " " + wordTwo)
+
+# Variables - Naming
+name = "Aaron"
+age = 28
+
