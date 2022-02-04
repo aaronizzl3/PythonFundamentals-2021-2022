34aa877bf46b0c30647e23e41f5d593c24ef6d4c
aaronizzl3 <58277262+aaronizzl3@users.noreply.github.com>
2022-02-04 11:35:04 +0000

    Create Selection.py

---
+++ 1 - Module Introduction/Selection.py
@@ -0,0 +1,27 @@
+
+# TODO: Green Milk is True
+GreenMilk = True
+
+if GreenMilk:
+    print("The milk is green.")
+else:
+    print("The milk is not green.")
+
+# TODO: Find largest number A / B
+A = 5
+B = 4
+
+if A > B:
+    print(f"{A} is larger than {B}")
+elif B > A:
+    print(f"{B} is larger than {A}")
+else:
+    print(f"{A} and {B} are equal.")
+
+# TODO: Not Equals to
+FavColour = "Orange"
+
+if FavColour != "Orange":
+    print("Your favourite colour isn't orange.")
+elif FavColour == "Orange":
+    print("You favourite colour is orange.")
