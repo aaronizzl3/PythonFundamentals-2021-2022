ecd1f29442215cd8e4af4a94adf90eff12abb202
aaronizzl3 <58277262+aaronizzl3@users.noreply.github.com>
2022-02-09 11:19:27 +0000

    Create Countdown.py

---
+++ 2 - Programming Paradigms/Countdown.py
@@ -0,0 +1,21 @@
+
+# TODO: Countdown from 60 to 0, and then say countdown complete
+
+x = 60
+
+while x > 0:
+    print(f"Count: {x}")
+    x -= 1
+
+print("Countdown complete!")
+
+
+# TODO: Count up to 5 and then say count complete
+
+y = 0
+
+while y < 6:
+    print(f"Count: {y}")
+    y += 1
+
+print("Second countdown complete.")
